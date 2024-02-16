f65a9c1c02ba20d2dc169cd1b63ec9a0ccec5e97
ehtishamalik <malikehtishamali334@hotmail.com>
2024-02-16 17:58:32 +0500

    Added Animal images for card

---
+++ src/components/common/images.ts
@@ -0,0 +1,107 @@
+import animal1 from "../../assets/animals/1.png";
+import animal2 from "../../assets/animals/2.png";
+import animal3 from "../../assets/animals/3.png";
+import animal4 from "../../assets/animals/4.png";
+import animal5 from "../../assets/animals/5.png";
+import animal6 from "../../assets/animals/6.png";
+import animal7 from "../../assets/animals/7.png";
+import animal8 from "../../assets/animals/8.png";
+import animal9 from "../../assets/animals/9.png";
+import animal10 from "../../assets/animals/10.png";
+import animal11 from "../../assets/animals/11.png";
+import animal12 from "../../assets/animals/12.png";
+import animal13 from "../../assets/animals/13.png";
+import animal14 from "../../assets/animals/14.png";
+import animal15 from "../../assets/animals/15.png";
+import animal16 from "../../assets/animals/16.png";
+import animal17 from "../../assets/animals/17.png";
+import animal18 from "../../assets/animals/18.png";
+import animal19 from "../../assets/animals/19.png";
+import animal20 from "../../assets/animals/20.png";
+import animal21 from "../../assets/animals/21.png";
+import animal22 from "../../assets/animals/22.png";
+import animal23 from "../../assets/animals/23.png";
+import animal24 from "../../assets/animals/24.png";
+import animal25 from "../../assets/animals/25.png";
+import animal26 from "../../assets/animals/26.png";
+import animal27 from "../../assets/animals/27.png";
+import animal28 from "../../assets/animals/28.png";
+import animal29 from "../../assets/animals/29.png";
+import animal30 from "../../assets/animals/30.png";
+import animal31 from "../../assets/animals/31.png";
+import animal32 from "../../assets/animals/32.png";
+import animal33 from "../../assets/animals/33.png";
+import animal34 from "../../assets/animals/34.png";
+import animal35 from "../../assets/animals/35.png";
+import animal36 from "../../assets/animals/36.png";
+import animal37 from "../../assets/animals/37.png";
+import animal38 from "../../assets/animals/38.png";
+import animal39 from "../../assets/animals/39.png";
+import animal40 from "../../assets/animals/40.png";
+import animal41 from "../../assets/animals/41.png";
+import animal42 from "../../assets/animals/42.png";
+import animal43 from "../../assets/animals/43.png";
+import animal44 from "../../assets/animals/44.png";
+import animal45 from "../../assets/animals/45.png";
+import animal46 from "../../assets/animals/46.png";
+import animal47 from "../../assets/animals/47.png";
+import animal48 from "../../assets/animals/48.png";
+import animal49 from "../../assets/animals/49.png";
+import animal50 from "../../assets/animals/50.png";
+
+import back from "../../assets/img/back.png";
+
+export const allAnimals = [
+  animal1,
+  animal2,
+  animal3,
+  animal4,
+  animal5,
+  animal6,
+  animal7,
+  animal8,
+  animal9,
+  animal10,
+  animal11,
+  animal12,
+  animal13,
+  animal14,
+  animal15,
+  animal16,
+  animal17,
+  animal18,
+  animal19,
+  animal20,
+  animal21,
+  animal22,
+  animal23,
+  animal24,
+  animal25,
+  animal26,
+  animal27,
+  animal28,
+  animal29,
+  animal30,
+  animal31,
+  animal32,
+  animal33,
+  animal34,
+  animal35,
+  animal36,
+  animal37,
+  animal38,
+  animal39,
+  animal40,
+  animal41,
+  animal42,
+  animal43,
+  animal44,
+  animal45,
+  animal46,
+  animal47,
+  animal48,
+  animal49,
+  animal50,
+];
+
+export const backImage = back;
