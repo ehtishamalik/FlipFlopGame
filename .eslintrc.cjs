2b50855b4bf246f29d8d154c30699ee2a69ab58b
ehtishamalik <malikehtishamali334@hotmail.com>
2024-02-17 23:49:34 +0500

    added support for lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
-}
+  rules: {
+    // Add any specific rules or overrides here
+    "react/react-in-jsx-scope": "off",
+    'prettier/prettier': 'error', // Use Prettier to format code
+  },
+};
