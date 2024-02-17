3f3f39180328a7eabc567e2af087e902d52defe8
ehtishamalik <malikehtishamali334@hotmail.com>
2024-02-17 21:53:03 +0500

    removed excessive code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { DifficultySelector } from "./components/DifficultySelector"
 
 function App() {
-  const levels = ['easy', 'medium', 'normal', 'expert'];
-  const onDS = (level: string) => {
-    console.log(level);
-  };
 
   return (
     <>
-      <DifficultySelector levels={levels} classNames="bg-gds" callback={onDS} />
     </>
   )
 }
