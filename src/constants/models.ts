0c838b24cce45b51b5f6d42bc6b8f4e9a3ce0bdd
ehtishamalik <malikehtishamali334@hotmail.com>
2024-02-20 17:23:51 +0500

    Added common constants and types

---
+++ src/constants/models.ts
@@ -0,0 +1,14 @@
+import { DifficultyLevel } from '../types'
+
+export const difficultyLevels: DifficultyLevel[] = [
+  'easy',
+  'normal',
+  'hard',
+  'expert',
+]
+export const dimensions = {
+  easy: 4,
+  normal: 6,
+  hard: 8,
+  expert: 10,
+}
