82fed8c4777c40db4ef459972f4f9ea5fe878897
ehtishamalik <malikehtishamali334@hotmail.com>
2024-02-28 16:45:37 +0500

    Added toastify for notification alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
+import { useState } from 'react'
+import { ToastContainer } from 'react-toastify'
 import { FlipFlopGame } from './components/FlipFlopGame'
 import { Timer } from './components/Timer'
 import { InputField } from './components/InputField'
 import { LeaderBoard } from './components/LeaderBoard'
-import { useState } from 'react'
 
 function App() {
   const [userName, setUserName] = useState<string | null>(null)
   const [isTimerActive, setIsTimerActive] = useState<boolean>(false)
 
   return (
-    <section className='animal-memory'>
-      <div className='container'>
-        <header className='header'>
-          <h1>animal memory</h1>
-        </header>
-        <div className='fields'>
-          <InputField callback={setUserName} />
-          <Timer isActive={isTimerActive} />
+    <>
+      <section className='animal-memory'>
+        <div className='container'>
+          <header className='header'>
+            <h1>animal memory</h1>
+          </header>
+          <div className='fields'>
+            <InputField callback={setUserName} />
+            <Timer isActive={isTimerActive} />
+          </div>
+          <FlipFlopGame
+            userName={userName}
+            onLevelChange={(level) => {
+              setIsTimerActive(false)
+              setTimeout(() => {
+                setIsTimerActive(level != null)
+              }, 500)
+            }}
+          />
         </div>
-        <FlipFlopGame
-          userName={userName}
-          onLevelChange={(level) => {
-            setIsTimerActive(false)
-            setTimeout(() => {
-              setIsTimerActive(level != null)
-            }, 500)
-          }}
-        />
-      </div>
-      <LeaderBoard header='leaderboard' />
-    </section>
+        <LeaderBoard header='leaderboard' />
+      </section>
+      <ToastContainer autoClose={4000} />
+    </>
   )
 }
 
--- src/components/FlipFlopGame/component.tsx
+++ src/components/FlipFlopGame/component.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
 import { difficultyLevels } from '../../constants'
 import { DifficultySelector } from '../DifficultySelector'
 import { getRandomArray } from './constant'
@@ -6,7 +7,6 @@ import { CardsArray, DifficultyLevel } from '../../types'
 import clsx from 'clsx'
 import { Cards } from '../Cards'
 import { FlipFlopGameProps } from './types'
-import { Alert } from '../Alert'
 import { saveTime } from './helper'
 
 export const FlipFlopGame = ({
@@ -14,7 +14,6 @@ export const FlipFlopGame = ({
   onLevelChange,
 }: FlipFlopGameProps) => {
   const [gameLevel, setGameLevel] = useState<DifficultyLevel | null>(null)
-  const [showAlert, setShowAlert] = useState<string | null>(null)
   const [cards, setCards] = useState<CardsArray>([])
 
   useEffect(() => {
@@ -24,68 +23,50 @@ export const FlipFlopGame = ({
   }, [gameLevel])
 
   return (
-    <>
-      {showAlert && (
-        <Alert
-          message={showAlert}
-          callback={() => {
-            setShowAlert(null)
-          }}
-        />
+    <div className='game-container'>
+      <DifficultySelector
+        levels={difficultyLevels}
+        defaultLevel={gameLevel}
+        callback={(level: DifficultyLevel) => {
+          if (userName) {
+            setGameLevel(level)
+            onLevelChange?.(level)
+          } else {
+            toast('Please enter your name')
+          }
+        }}
+      />
+      {gameLevel ? (
+        <div
+          className={clsx('cards-grid', {
+            'grid-4': gameLevel === 'easy',
+            'grid-6': gameLevel === 'normal',
+            'grid-8': gameLevel === 'hard',
+            'grid-10': gameLevel === 'expert',
+          })}
+        >
+          <Cards
+            cards={cards}
+            onGameComplete={() => {
+              const timeout = setTimeout(() => {
+                toast('Congratulations, You have WON!')
+                saveTime(gameLevel, userName!)
+                setGameLevel(null)
+                onLevelChange?.(null)
+                clearTimeout(timeout)
+              }, 1000)
+            }}
+          />
+        </div>
+      ) : (
+        <>
+          <h2>Welcome to Animal Memory!</h2>
+          <p>
+            Ready to test your memory? Enter your name and start matching
+            adorable animal pairs.<br/> The clock is ticking, so have fun!
+          </p>
+        </>
       )}
-      <div className='game-container'>
-        <DifficultySelector
-          levels={difficultyLevels}
-          defaultLevel={gameLevel}
-          callback={(level: DifficultyLevel) => {
-            if (userName) {
-              setGameLevel(level)
-              onLevelChange?.(level)
-            } else {
-              setShowAlert('Please enter your name')
-            }
-          }}
-        />
-        {gameLevel ? (
-          <div
-            className={clsx('cards-grid', {
-              'grid-4': gameLevel === 'easy',
-              'grid-6': gameLevel === 'normal',
-              'grid-8': gameLevel === 'hard',
-              'grid-10': gameLevel === 'expert',
-            })}
-          >
-            <Cards
-              cards={cards}
-              onGameComplete={() => {
-                const timeout = setTimeout(() => {
-                  setShowAlert('Congratulations, You have WON!')
-                  saveTime(gameLevel, userName!)
-                  setGameLevel(null)
-                  onLevelChange?.(null)
-                  clearTimeout(timeout)
-                }, 1000)
-              }}
-            />
-          </div>
-        ) : (
-          <>
-            <h2>Welcome to Animal Memory, a memory game for your memory :D</h2>
-            <p>
-              You might be asking yourself how to play this game. Do not worry,
-              that is why this paragraph is here.
-            </p>
-            <p>
-              Please, insert your name so you can start the game. Once you
-              start, the timer will start ticking.
-            </p>
-            <p>
-              When you are done, it will stop the time position and the username
-              will be printed to the table. ;)
-            </p>
-          </>
-        )}
-      </div>
-    </>
+    </div>
   )
 }
