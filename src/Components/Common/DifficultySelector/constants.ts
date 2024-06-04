c5bb4a89ece79600ff4e0fad532865586fff7b7f
ehtishamalik <malikehtishamali334@hotmail.com>
2024-06-04 15:54:21 +0500

    Added difficulty selector with styles

---
+++ src/Components/Common/DifficultySelector/constants.ts
@@ -0,0 +1,20 @@
+import { diffultyOptionsProps } from './types';
+
+export const diffultyOptions: diffultyOptionsProps = [
+  {
+    difficulty: 'easy',
+    gridSize: 4,
+  },
+  {
+    difficulty: 'medium',
+    gridSize: 6,
+  },
+  {
+    difficulty: 'hard',
+    gridSize: 8,
+  },
+  {
+    difficulty: 'expert',
+    gridSize: 10,
+  },
+];
---
+++ src/Components/Common/DifficultySelector/types.ts
@@ -0,0 +1,7 @@
+export type DifficultySelectorProps = {
+  callback: (level: string) => void
+}
+export type diffultyOptionsProps = {
+  difficulty: string
+  gridSize: number
+}[]
