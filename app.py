4fd17728c1d19451a63476b4da5c3144e211aa9c
ehtishamalik <malikehtishamali334@hotmail.com>
2024-06-06 22:51:32 +0500

    tested session storage

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, session
+import datetime
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "EHTISHAM"
+app.permanent_session_lifetime = datetime.timedelta(seconds=5)
 
 @app.route('/')
 def index():
@@ -9,6 +13,25 @@ def index():
 @app.route('/abc')
 def abc():
     return '<h1>Hello From H1</h1>'
+
+# Sessions in flask
+@app.route('/spark-counter/')
+def spark():
+    if 'spark' in session:
+        session['spark'] = session.get('spark') + 1  # reading and updating session data
+    else:
+        session['spark'] = 1 # setting session data
+    return "Total visits: {}".format(session.get('spark'))
+
+@app.route('/delete-spark/')
+def delete_spark():
+    session.pop('spark', None) # delete spark
+    return 'spark deleted'
+
+@app.route('/items/')
+def items():
+    res = str(session.items())
+    return res
 
 if __name__ == '__main__':
     app.run(debug=True)
