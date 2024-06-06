c1977198345d1dbcd8ea26f8a920a662d33ad846
ehtishamalik <malikehtishamali334@hotmail.com>
2024-06-06 15:08:13 +0500

    Added card component and updated FlipFlop component with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom';
-import { NavBar } from './Components/Common/NavBar';
+// import { Outlet } from 'react-router-dom';
+// import { NavBar } from './Components/Common/NavBar';
+import { FlipFlop } from './Pages/FlipFlop';
 
 function App() {
   return (
     <div className="container">
-      <NavBar />
-      <Outlet />
+      {/* <NavBar /> */}
+      {/* <Outlet /> */}
+      <FlipFlop />
     </div>
   );
 }
--- src/Pages/FlipFlop/component.tsx
+++ src/Pages/FlipFlop/component.tsx
@@ -1,1 +1,15 @@
-export function FlipFlop() {}
+import { Card } from '../../Components/FlipFlop/Card';
+
+export function FlipFlop() {
+  return (
+    <section className="flip-flop">
+      <div className="flip-flop__container">
+        <div className="flip-flop__grid">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 900].map((index) => (
+            <Card imageLink={`static/${index}.png`} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
